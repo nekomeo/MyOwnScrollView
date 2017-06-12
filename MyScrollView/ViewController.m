41274eaa18291af226e467bd5113a104ced36d8b
nekomeo <linh.tu545@gmail.com>
2017-06-12 15:04:25 -0700

    Create new scroll view to fix origin issue

--- MyScrollView/ViewController.m
+++ MyScrollView/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+//@property (nonatomic) UIView *view;
+@property (nonatomic) UIScrollView *scrollView;
+
 
 @end
 
@@ -17,13 +20,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view = [[UIView alloc] initWithFrame:self.view.frame];
+    [self.view addSubview:self.view];
+//    CGRect offScreenFrame = self.view.bounds;
+//    offScreenFrame.origin = CGPointMake(0, CGRectMake(self.view.frame));
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+//- (void)didReceiveMemoryWarning {
+//    [super didReceiveMemoryWarning];
+//    // Dispose of any resources that can be recreated.
+//}
+
+- (void)scrollingView
+{
+    self.scrollView = [[UIScrollView alloc] initWithFrame:self.view.frame];
+//    [self.view addSubview:self.scrollView];
+    
 }
+
+//- (void)viewDidAppear:(BOOL)animated
+//{
+//    [super viewDidAppear:animated];
+//    self.scrollView.contentSize = CGRectMake(100, 200, 400, 750);
+//    [self.view addSubview:self.scrollView];
+//}
 
 
 @end
