1821491c594b0a18438cf73e003f83be0fbac362
nekomeo <linh.tu545@gmail.com>
2017-06-12 20:00:51 -0700

    Recode color views

--- MyScrollView/ViewController.m
+++ MyScrollView/ViewController.m
@@ -11,7 +11,10 @@
 @interface ViewController ()
 //@property (nonatomic) UIView *view;
 @property (nonatomic) UIScrollView *scrollView;
-
+@property (nonatomic) UIView *redView;
+@property (nonatomic) UIView *greenView;
+@property (nonatomic) UIView *blueView;
+@property (nonatomic) UIView *yellowView;
 
 @end
 
@@ -20,31 +23,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.view = [[UIView alloc] initWithFrame:self.view.frame];
-    [self.view addSubview:self.view];
-//    CGRect offScreenFrame = self.view.bounds;
-//    offScreenFrame.origin = CGPointMake(0, CGRectMake(self.view.frame));
-}
 
-
-//- (void)didReceiveMemoryWarning {
-//    [super didReceiveMemoryWarning];
-//    // Dispose of any resources that can be recreated.
-//}
-
-- (void)scrollingView
-{
-    self.scrollView = [[UIScrollView alloc] initWithFrame:self.view.frame];
-//    [self.view addSubview:self.scrollView];
+    self.redView = [[UIView alloc] initWithFrame:CGRectMake(20, 20, 100, 100)];
+    self.redView.backgroundColor = [UIColor redColor];
+    [self.view addSubview:self.redView];
     
+    self.greenView = [[UIView alloc] initWithFrame:CGRectMake(150, 150, 150, 200)];
+    self.greenView.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:self.greenView];
+    
+    self.blueView = [[UIView alloc] initWithFrame:CGRectMake(40, 400, 200, 150)];
+    self.blueView.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:self.blueView];
+    
+    self.yellowView = [[UIView alloc] initWithFrame:CGRectMake(100, 600, 180, 150)];
+    self.yellowView.backgroundColor = [UIColor yellowColor];
+    [self.view addSubview:self.yellowView];
 }
 
-//- (void)viewDidAppear:(BOOL)animated
-//{
-//    [super viewDidAppear:animated];
-//    self.scrollView.contentSize = CGRectMake(100, 200, 400, 750);
-//    [self.view addSubview:self.scrollView];
-//}
 
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
 
 @end
